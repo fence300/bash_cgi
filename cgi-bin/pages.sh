7a87535a28459f3a28ffab960a548d8183ce9848
Luke Kreft <luke@archbook.mirkre.com>
2017-12-25 03:03:20 -0500

    added

---
+++ cgi-bin/pages.sh
@@ -0,0 +1,22 @@
+function environment_page ()
+{
+  echo "Content-Type: text/html"
+  echo
+  echo "<html>"
+  echo "<head>"
+  echo "<title>New Test Page</title>"
+  echo "<link rel='stylesheet' type='text/css' href='/style.css' >"
+  echo "</head>"
+  echo "<body>"
+  echo "<div class='container'>"
+
+  echo "<div class='title'>Environment</div>"
+  env | sort | while read line
+  do
+    echo "<p>$line</p>"
+  done
+
+  echo "</div>"
+  echo "</body>"
+  echo "</html>"
+}
