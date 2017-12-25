509ea47fc95c23c7f3d77e21d3fecce6b4b3b71d
Luke Kreft <luke@archbook.mirkre.com>
2017-12-25 01:02:25 -0500

    update

--- pages.sh
+++ pages.sh
@@ -1,1 +1,22 @@
-#!/bin/bash
+function environment_page ()
+{
+  echo "Content-Type: text/html"
+  echo
+  echo "<html>"
+  echo "<head>"
+  echo "<title>New Test Page</title>"
+  echo "<link rel='stylesheet' type='text/css' href='style.css' />"
+  echo "</head>"
+  echo "<body>"
+  echo "<div class='container'>"
+
+  echo "<div class='title'>Environment</div>"
+  env | sort | while read line
+  do
+    echo "<p>$line</p>"
+  done
+
+  echo "</div>"
+  echo "</body>"
+  echo "</html>"
+}
